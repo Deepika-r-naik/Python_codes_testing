480aa0569246dac5bba0615398523f7126050dfc
Deepika <95289716+Deepika-r-naik@users.noreply.github.com>
2023-06-20 16:36:02 +0530

    Update script1.py

--- script1.py
+++ script1.py
@@ -31,3 +31,46 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+    
+    
+    #To store the UID's in a list
+    
+    import requests
+
+def fetch_data(url, headers={}, params={}):
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+
+        uids = []  # Create an empty list to store the uids
+
+        if response.status_code == 200:
+            data = response.json()
+
+            if isinstance(data, list):
+                for item in data:
+                    uid = item.get('uid')
+                    uids.append(uid)  # Append uid to the list
+
+            else:
+                print("Response data is not a list.")
+
+        else:
+            print(f"Request was not successful. Status code: {response.status_code}")
+
+        return uids  # Return the list of uids
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+
+def main():
+    url = "https://test"
+    headers = {'Authorization': 'Basic .........'}
+    params = {'param1': 'value1', 'param2': 'value2'}
+
+    uids = fetch_data(url, headers=headers, params=params)
+    print(uids)  # Print the list of uids
+
+if __name__ == "__main__":
+    main()
