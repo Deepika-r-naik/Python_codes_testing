5a4505d5ff522473697b571e4ce0617069590295
Deepika <95289716+Deepika-r-naik@users.noreply.github.com>
2023-06-23 20:26:27 +0530

    Update keyvalues1.py

--- keyvalues1.py
+++ keyvalues1.py
@@ -110,4 +110,62 @@ def main():
 
 if __name__ == "__main__":
     main()
+#############################################################
+
+import requests
+
+def fetch_data():
+    url = "https://test"
+    headers = {'Authorization': 'Basic .........'}
+    params = {'param1': 'value1', 'param2': 'value2'}
+
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+
+        cases = []  # Create an empty list to store case dictionaries
+        uri = url
+        result = None
+
+        if response.status_code == 200:
+            result = response.json()
+
+            # Extract values from result
+            group_uuid = result.get('groupUUID')
+            serial_number = result.get('serialNumber')
+            component_id = result.get('componentID')
+            ticket_type = result.get('ticketType')
+            case_type = result.get('Type')
+            state = result.get('state')
+
+            if state != 'canceled':
+                cases.append({
+                    'groupUUID': group_uuid,
+                    'serialNumber': serial_number,
+                    'componentID': component_id,
+                    'ticketType': ticket_type,
+                    'Type': case_type,
+                    'state': state
+                })
+
+            if "@odata.nextLink" in result.keys():
+                uri = f"https://{ome_instance}" + result['@odata.nextLink']
+
+        else:
+            print(f"Request was not successful. Status code: {response.status_code}")
+
+        return cases, uri
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+
+def main():
+    cases, uri = fetch_data()
+    print("Cases:")
+    for case in cases:
+        print(case)
+    print("URI:", uri)
+
+if __name__ == "__main__":
+    main()
 
