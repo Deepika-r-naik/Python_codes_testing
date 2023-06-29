68e7affd74d2e5e0d38c30947891f648c7145c1e
Deepika <95289716+Deepika-r-naik@users.noreply.github.com>
2023-06-29 22:15:22 +0530

    Update NewAPITest1.py

--- NewAPITest1.py
+++ NewAPITest1.py
@@ -160,4 +160,44 @@ def main():
 
 if __name__ == "__main__":
     main()
+#################################################################################
+
+import requests
+
+def fetch_data(url, headers={}, params={}):
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+
+        content_type = response.headers.get("Content-Type", "")
+        if "application/json" in content_type:
+            data = response.json()
+            print(json.dumps(data, indent=4))  # Print data in JSON format
+        else:
+            response_text = response.text
+            data = {}
+
+            # Adjust the parsing logic to match your response text format
+            json_data = json.loads(response_text)
+            metadata = json_data.get("_metadata", {})
+            attributes = metadata.get("attributes", {})
+
+            for key, value in attributes.items():
+                if key in ["PmrUID", "state", "PMRType", "eventID", "sourceName", "componentName"]:
+                    data[key] = value
+
+            print(data)
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+
+def main():
+    url = "https://test"
+    headers = {'Authorization': 'Basic .........'}
+    params = {'param1': 'value1', 'param2': 'value2'}
+
+    fetch_data(url, headers=headers, params=params)
+
+if __name__ == "__main__":
+    main()
 
