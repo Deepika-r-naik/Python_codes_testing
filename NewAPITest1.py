4d1276ddefbca052e4a1ea1038846f48f978fcb1
Deepika <95289716+Deepika-r-naik@users.noreply.github.com>
2023-06-29 21:18:24 +0530

    Update NewAPITest1.py

--- NewAPITest1.py
+++ NewAPITest1.py
@@ -77,3 +77,44 @@ def main():
 if __name__ == "__main__":
     main()
 #################################################### Parsing text format to Json and then storing values in dictionary
+
+
+import requests
+
+def fetch_data(url, headers={}, params={}):
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+
+        content_type = response.headers.get("Content-Type", "")
+        if "application/json" in content_type:
+            data = response.json()
+            print(json.dumps(data, indent=4))  # Print data in JSON format
+        else:
+            response_text = response.text
+            data = {}
+
+            # Adjust the parsing logic to match your response text format
+            # Example: Parsing text in the format "PmrUID:value1, state:value2, PMRType:value3, eventID:value4, sourceName:value5, componentName:value6"
+            pairs = response_text.split(", ")
+            for pair in pairs:
+                key, value = pair.split(":")
+                # Check if the key is one of the specified attributes
+                if key in ["PmrUID", "state", "PMRType", "eventID", "sourceName", "componentName"]:
+                    data[key] = value
+
+            print(data)
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+
+def main():
+    url = "https://test"
+    headers = {'Authorization': 'Basic .........'}
+    params = {'param1': 'value1', 'param2': 'value2'}
+
+    fetch_data(url, headers=headers, params=params)
+
+if __name__ == "__main__":
+    main()
+
