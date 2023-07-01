4621c1b96d3396405cdacacc23e46bd9e4ba4cf2
Deepika <95289716+Deepika-r-naik@users.noreply.github.com>
2023-07-01 23:04:47 +0530

    Update Test4.py

--- Test4.py
+++ Test4.py
@@ -60,3 +60,33 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+###########################################################################################
+if "application/json" in content_type:
+                data = response.json()
+                if isinstance(data, list):
+                    filtered_data = [ticket for ticket in data if ticket.get("state") != "Closed"]
+                    print(json.dumps(filtered_data, indent=4))  # Print filtered data in JSON format
+                    
+                    for ticket in filtered_data:
+                        ticket_uri = ticket.get("uri")
+                        if ticket_uri:
+                            ticket_details = requests.get(ticket_uri, headers=headers)
+                            if ticket_details.status_code == 200:
+                                ticket_data = ticket_details.json()
+                                print(json.dumps(ticket_data, indent=4))  # Print ticket details in JSON format
+                            else:
+                                print(f"Failed to retrieve ticket details. Status code: {ticket_details.status_code}")
+                        else:
+                            print("Ticket URI not found.")
+                else:
+                    print("Data is not in the expected format.")
+            else:
+                print("Data is not in JSON format.")
+        else:
+            print(f"Request was not successful. Status code: {response.status_code}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+
