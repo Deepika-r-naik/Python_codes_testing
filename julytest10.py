b6153748762031235950f866e20925c54e2db809
Deepika <95289716+Deepika-r-naik@users.noreply.github.com>
2023-07-02 17:22:49 +0530

    Update julytest10.py

--- julytest10.py
+++ julytest10.py
@@ -31,3 +31,19 @@ if response.status_code == 200:
 
 else:
     print('Failed to fetch ticket details. Status code:', response.status_code)
+
+###Create a dictionary with specific attributes
+
+    filtered_tickets = {}
+    for ticket in tickets:
+        filtered_ticket = {
+            "id": ticket["id"],
+            "eventID": ticket["eventID"],
+            "componentName": ticket["componentName"],
+            "createDate": ticket["createDate"],
+            "PmrUID": ticket["PmrUID"],
+            "state": ticket["state"],
+            "ticketType": ticket["ticketType"],
+            "PMRType": ticket["PMRType"]
+        }
+        filtered_tickets[ticket["id"]] = filtered_ticket
