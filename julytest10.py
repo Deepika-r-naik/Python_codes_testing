cf407899e11c6c6f2961d9a4894949ccba308d5a
Deepika <95289716+Deepika-r-naik@users.noreply.github.com>
2023-07-02 17:14:48 +0530

    Create julytest10.py

---
+++ julytest10.py
@@ -0,0 +1,33 @@
+import requests
+
+# Make API call to fetch ticket details
+response = requests.get('https://api.example.com/tickets')
+
+# Check if the API call was successful
+if response.status_code == 200:
+    # Convert the response JSON to a dictionary
+    response_data = response.json()
+
+    # Get the list of tickets from the "results" key
+    tickets = response_data["results"]
+
+    # Extract specific attributes from tickets
+    extracted_tickets = []
+    for ticket in tickets:
+        extracted_ticket = {
+            "id": ticket["id"],
+            "eventID": ticket["eventID"],
+            "componentName": ticket["componentName"],
+            "createDate": ticket["createDate"],
+            "PmrUID": ticket["PmrUID"],
+            "state": ticket["state"],
+            "ticketType": ticket["ticketType"],
+            "PMRType": ticket["PMRType"]
+        }
+        extracted_tickets.append(extracted_ticket)
+
+    # Perform further operations on the extracted_tickets
+    # ...
+
+else:
+    print('Failed to fetch ticket details. Status code:', response.status_code)
